6f725c08cd55335f891002fea2f6cc43f001e614
Dimple Janardhan <djanardh@2-E-6.42heilbronn.de>
2025-03-18 21:43:27 +0100

    updated split

--- ft_split.c
+++ ft_split.c
@@ -6,27 +6,27 @@
 /*   By: djanardh <djanardh@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/14 18:03:31 by djanardh          #+#    #+#             */
-/*   Updated: 2025/03/16 21:45:14 by djanardh         ###   ########.fr       */
+/*   Updated: 2025/03/18 21:39:54 by djanardh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// Parameters: s: The string to be split.
-//             c: The delimiter character.
+// // Parameters: s: The string to be split.
+// //             c: The delimiter character.
 
-// The array of new strings resulting from the split.NULL if the allocation
-//  fails.
+// // The array of new strings resulting from the split.NULL if the allocation
+// //  fails.
 
-// 	Allocates memory(using malloc(3)) and
-// 	returns an array of strings obtained by splitting ’s’ using the character
-//     ’c’ as a delimiter.The array must end with a NULL pointer.
+// // 	Allocates memory(using malloc(3)) and
+// // 	returns an array of strings obtained by splitting ’s’ using the character
+// //     ’c’ as a delimiter.The array must end with a NULL pointer.
 
-// Notes for myself:
-// split_strings[count_array] = ft_substr(s, 0, ((end_index-start_index) + 2));
-// extra +1 coz null terminating the individual string?
+// // Notes for myself:
+// // split_strings[count_array] = ft_substr(s, 0, ((end_index-start_index) + 2));
+// // extra +1 coz null terminating the individual string?
 
 #include "libft.h"
 
-static size_t	ft_word_count(const char *s, char c)
+static unsigned int	ft_word_count(const char *s, char c)
 {
 	size_t	count;
 	size_t	i;
@@ -35,7 +35,7 @@ static size_t	ft_word_count(const char *s, char c)
 	count = 0;
 	while (s[i] != '\0')
 	{
-		if ((s[i] == c) && (s[i + 1] != c))
+		if (s[i] != c && s[i + 1] == c && s[i + 2] != '\0')
 		{
 			count++;
 		}
@@ -44,106 +44,187 @@ static size_t	ft_word_count(const char *s, char c)
 	return (count + 1);
 }
 
-static size_t	ft_end_index(unsigned int start_index, const char *s, char c)
+static void	ft_free_memory(char **split_strings, size_t count_array)
 {
-	while (s[start_index + 1] != '\0')
+	while (count_array--)
 	{
-		if (s[start_index + 1] != c && (s[start_index + 2] == c || s[start_index
-				+ 2] == '\0'))
-			return (start_index + 1);
-		start_index++;
-	}
-	return (start_index);
-}
-
-static void	ft_free_memory(char **split_strings, size_t total_word_count)
-{
-	size_t	i;
-
-	i = 0;
-	while (i < total_word_count)
-	{
-		free(split_strings[i]);
-		i++;
+		free(split_strings[count_array]);
 	}
 	free(split_strings);
-}
-
-static char	**ft_strings_split(size_t i, char c, char const *s,
-		char **split_strings, unsigned int count_array, size_t total_word_count)
-{
-	size_t	start_index;
-	size_t	end_index;
-
-	while (s[i] != '\0')
-	{
-		if (s[i] == c && s[i + 1] != c)
-		{
-			start_index = i + 1;
-			end_index = ft_end_index(i + 1, s, c);
-			split_strings[count_array] = ft_substr(s, start_index, ((end_index
-							- start_index) + 1));
-			if (split_strings[count_array] == NULL)
-			{
-				ft_free_memory(split_strings, total_word_count);
-				return (NULL);
-			}
-			count_array++;
-		}
-		i++;
-	}
-	split_strings[total_word_count] = NULL;
-	return (split_strings);
 }
 
 char	**ft_split(char const *s, char c)
 {
 	char			**split_strings;
-	size_t			i;
-	size_t			total_word_count;
 	unsigned int	end_index;
+	unsigned int	start_index;
 	unsigned int	count_array;
+	unsigned int	i;
 
 	i = 0;
 	count_array = 0;
-	if (s == NULL)
+	if (!s) {
+		printf("I am here!");
 		return (NULL);
-	total_word_count = ft_word_count(s, c);
-	split_strings = malloc((total_word_count + 1) * sizeof(char *));
+	}
+	if (s[0] == '\0')
+	{
+		split_strings = malloc(sizeof(char *));
+		split_strings[0] = "\0";
+		return (split_strings);
+	}
+	split_strings = malloc((ft_word_count(s, c) + 1) * sizeof(char *));
 	if (split_strings == NULL)
 		return (NULL);
-	if (s[0] != c)
+	printf("I am here!2");
+	printf("%s\n", s);
+	while (s[i] != '\0' && i == ft_strlen(s))
 	{
-		while (s[i] != '\0')
+		if (s[i] != c)
 		{
-			if (s[i + 1] == c && s[i + 2] != c)
+			start_index = i;
+			while (s[i] != '\0' && i == ft_strlen(s))
 			{
-				end_index = i;
-				split_strings[count_array] = ft_substr(s, 0, end_index + 1);
-				if (split_strings[count_array] == NULL)
-					ft_free_memory(split_strings, total_word_count);
-				count_array++;
+				if (s[i + 1] == c || s[i + 1] == '\0')
+				{
+					end_index = i;
+					split_strings[count_array] = ft_substr(s, start_index,
+						(end_index - start_index) + 1);
+					printf("%s\n", split_strings[count_array]);
+					if (split_strings[count_array] == NULL)
+						printf("I am here!2");
+						return (ft_free_memory(split_strings, count_array),
+							NULL);
+					count_array++;
+					break ;
+				}
 				i++;
-				break ;
 			}
-			i++;
 		}
+		i++;
 	}
-	return (ft_strings_split(i, c, s, split_strings, count_array,
-			total_word_count));
+	split_strings[ft_word_count(s, c)] = "\0";
+	return (split_strings);
 }
 
 int	main(void)
 {
-	const char *s = "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultricies diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.";
-	char c = 'i';
+	// const char *s = "      split       this for   me  !       ";
+	const char *s = "afgfgag sgdfgsdf fgsgdfdsg";
+	// const char *s = "\0aa\0bbb";
+	char c = ' ';
+	// printf("%u\n", ft_word_count(s, c));
 	char **result = ft_split(s, c);
 	unsigned int i = 0;
-	while (result[i] != NULL)
+	while (!(result[i]))
 	{
 		printf("split_string[%u] = %s\n", i, result[i]);
 		i++;
 	}
-	printf("split_string[%u] = %s\n", i, result[i]);
+	// printf("split_string[%u] = %s\n", i, result[i]);
+
+	// char **s = ft_split("lorem   ipsum dolor     sit amet, consectetur   adipiscing elit. Sed non risus. Suspendisse   ", ' ');
 	return (0);
 }
+
+// =========================================================================================
+
+// static int	ft_count_words_delimited(char const *str, char c)
+// {
+// 	size_t	count;
+// 	size_t	iter;
+
+// 	count = 0;
+// 	iter = 0;
+// 	while (str[iter] != '\0')
+// 	{
+// 		if (str[iter] != c)
+// 		{
+// 			count++;
+// 			while (str[iter] && str[iter] != c)
+// 				iter++;
+// 			if (str[iter] == '\0')
+// 				return (count);
+// 		}
+// 		iter++;
+// 	}
+// 	return (count);
+// }
+
+// static void	free_all(char **word)
+// {
+// 	int	i;
+
+// 	i = 0;
+// 	while (word[i])
+// 	{
+// 		free(word[i]);
+// 		i++;
+// 	}
+// 	free(word);
+// }
+
+// static int	split_loop(char const *s, char c, char **word)
+// {
+// 	int	len;
+// 	int	iter;
+
+// 	iter = 0;
+// 	while (*s)
+// 	{
+// 		if (*s != c)
+// 		{
+// 			len = 0;
+// 			while (*s && *s != c && ++len)
+// 				++s;
+// 			word[iter] = ft_substr(s - len, 0, len);
+// 			if (!word[iter])
+// 			{
+// 				free_all(word);
+// 				return (0);
+// 			}
+// 			iter++;
+// 			if (*s == '\0')
+// 				break ;
+// 		}
+// 		++s;
+// 	}
+// 	word[iter] = 0;
+// 	return (1);
+// }
+
+// char	**ft_split(char const *s, char c)
+// {
+// 	char	**word;
+// 	int		result;
+
+// 	if (!s)
+// 		return (NULL);
+// 	word = (char **) malloc(sizeof(char *)
+// 			* (ft_count_words_delimited(s, c) + 1));
+// 	if (word == NULL)
+// 		return (0);
+// 	result = split_loop(s, c, word);
+// 	if (result == 0)
+// 		return (NULL);
+// 	return (word);
+// }
+
+// int	main(void)
+// {
+// 	// const char *s = "      split       this for   me  !       ";
+// 	const char *s = "\0aa\0bbb";
+// 	char c = '\0';
+// 	// printf("%u\n", ft_word_count(s, c));
+// 	char **result = ft_split(s, c);
+// 	unsigned int i = 0;
+// 	while (result[i] != NULL)
+// 	{
+// 		printf("split_string[%u] = %s\n", i, result[i]);
+// 		i++;
+// 	}
+// 	// printf("split_string[%u] = %s\n", i, result[i]);
+
+// 	// char **s = ft_split("lorem   ipsum dolor     sit amet, consectetur   adipiscing elit. Sed non risus. Suspendisse   ", ' ');
+// 	return (0);
+// }
